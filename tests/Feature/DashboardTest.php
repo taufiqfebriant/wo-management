2a20e6f3e4d6919fd8ad694d906767ec7e435869
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2025-03-01 21:54:15 +0700

    init

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\User;
+
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+test('guests are redirected to the login page', function () {
+	$this->get('/')->assertRedirect('/login');
+});
+
+test('authenticated users can visit the dashboard', function () {
+	$this->actingAs($user = User::factory()->create());
+
+	$this->get('/')->assertOk();
+});
